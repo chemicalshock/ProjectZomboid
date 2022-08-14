8b87b1060c76333f8226f86ddfe0d344f3ca692f
Colin J.D. Stewart <81357919+PiXeL-GaMMa@users.noreply.github.com>
2022-08-15 00:46:04 +0300

    Update clearSkillLevels.lua

--- snippets/clearSkillLevels.lua
+++ snippets/clearSkillLevels.lua
@@ -1,20 +1,19 @@
 --//
 --// Clear all skills and xp
---// Author: Colin J.D. Stewart | Updated: 14.08.2022
+--// Author: Colin J.D. Stewart | Updated: 15.08.2022
 --// 
 local function clearSkills(player)
 	local pl = PerkFactory.PerkList;
 	local xp = player:getXp();
 	
 	for i = 0, pl:size()-1 do
-		local info = pl:get(i);
-		local pk = info:getType();		
-		local curLevel = player:getPerkLevel(pk);
+		local pk = pl:get(i):getType();		
 		
-		if curLevel > 0 then
-			player:level0(pk);
-		end;
-		
+		player:level0(pk);	--// Thanks Hugo Qwerty		
 		xp:setXPToLevel(pk, player:getPerkLevel(pk));
+		
+		--//Thanks Tchernobill
+		xp:setPerkBoost(pk, 0);
+		xp:getMultiplierMap():remove(pk);
 	end;
 end;
